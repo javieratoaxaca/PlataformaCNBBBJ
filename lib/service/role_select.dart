c7ff4775f190ef8808ec44be72a4bfa3ae25934d
F-AntonioRC <l20920144@voaxaca.tecnm.mx>
2025-03-03 14:41:57 -0600

    Cambios en archivos y actualizacion de logos/imagenes

--- lib/service/role_select.dart
+++ lib/service/role_select.dart
@@ -6,7 +6,6 @@ static Future<bool> isAdmin() async {
   final user = FirebaseAuth.instance.currentUser;
   if (user != null) {
     final idTokenResult = await user.getIdTokenResult();
-    print("Claims del usuario: ${idTokenResult.claims}");
     return idTokenResult.claims?['admin'] == true;
   }
   return false;
