4710e0465a921b57b09a410bc50d34f40a00179f
F-AntonioRC <l20920144@voaxaca.tecnm.mx>
2025-03-05 08:56:35 -0600

    Se eliminaron los archivos que son de la UI y métodos del administrador

--- lib/auth/auth_gate.dart
+++ lib/auth/auth_gate.dart
@@ -1,8 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:plataformacnbbbjo/auth/login_or_register.dart';
-import 'package:plataformacnbbbjo/pages/main_pages/home_page.dart';
-import 'package:plataformacnbbbjo/service/role_select.dart';
 import 'package:plataformacnbbbjo/userNormal/pages/home_normal.dart';
 
 class AuthGate extends StatelessWidget {
@@ -10,31 +6,6 @@ class AuthGate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<User?>(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-
-            if (snapshot.hasData) {
-              return FutureBuilder<bool>(
-                  future: RoleService.isAdmin(),
-                  builder: (context, adminSnapshot) {
-                    if (adminSnapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    if (adminSnapshot.hasData && adminSnapshot.data == true) {
-                      return const HomePage();
-                    } else {
-                      return const HomeNormal();
-                    }
-                  });
-            } else {
-              return const LoginOrRegister();
-            }
-          }),
-    );
+    return HomeNormal();
   }
 }
